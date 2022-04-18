cea24526eac6e6aeb386760ab15bc37828b63861
Adam Stirtan <adamstirtan@gmail.com>
2022-04-18 10:11:10 -0400

    Changed class name

--- TeamCitySetup.Tests/MyAwesomeTests.cs
+++ TeamCitySetup.Tests/MyAwesomeTests.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace TeamCitySetup.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class MyAwesomeTests
     {
         [TestMethod]
         public void TestMethod1()
